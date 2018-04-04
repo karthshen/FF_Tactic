bb7ba1ae0f76cfff97ed252415164f4bd14513fb
J. Shen <karth-shen@hotmail.com>
2018-04-04 17:07:53 -0400

    Added control and credit scene

---
+++ Assets/Scripts/UI/ControlMenuUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControlMenuUI : MonoBehaviour
+{
+
+	public GameObject ControlMenuCanvas;
+
+	// Use this for initialization
+	void Start ()
+	{
+		ControlMenuCanvas.SetActive (false);
+	}
+
+	public void ControlButton ()
+	{
+		ControlMenuCanvas.SetActive (true);
+	}
+
+	public void Resume ()
+	{
+		ControlMenuCanvas.SetActive (false);
+	}
+}
---
+++ Assets/Scripts/UI/CreditMenuUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditMenuUI : MonoBehaviour
+{
+
+	public GameObject CreditMenuCanvas;
+
+	// Use this for initialization
+	void Start ()
+	{
+		CreditMenuCanvas.SetActive (false);
+	}
+
+	public void CreditButton ()
+	{
+		CreditMenuCanvas.SetActive (true);
+	}
+
+	public void Resume ()
+	{
+		CreditMenuCanvas.SetActive (false);
+	}
+}
